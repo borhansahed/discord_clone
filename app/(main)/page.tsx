b7d9aee3f86d0ad7e447eb9013646b5ec3341178
borhansahed <mdsahed1099@gmail.com>
2023-08-28 12:15:26 +0600

    [DONE] dark-mode setup

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,13 +1,12 @@
+import { ModeToggle } from "@/components/mode/Mode";
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <>
-      <p className="text-4xl text-blue-500 font-bold">Hello Discord Clone</p>
-      <Button>Hello</Button>
-
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/provider/theme-provider";
+import { cn } from "@/lib/utils";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -18,7 +20,16 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={font.className}>{children}</body>
+        <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="light"
+            enableSystem
+            storageKey="discord-theme"
+          >
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
